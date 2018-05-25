0c612efca14bfbdf203ad67b966c8888fb9b41a3
natenhagen <natepappenhagen@gmail.com>
2018-05-25 10:19:46 -0600

    create game logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
-console.log("Up and running!");
+console.log("HI");
+
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+////////////////////////////////
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
 
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("user flipped " + cardOne + " & " + cardTwo);
+console.log("user flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
+	} else {
+		alert("Sorry, try again");
+	}
+}
 
